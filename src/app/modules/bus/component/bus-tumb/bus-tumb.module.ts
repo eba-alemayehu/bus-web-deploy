ddca3af5cf0b98ff84387d2bd7f85f32bd15ba1e
eba-alemayehu <31594849+eba-alemayehu@users.noreply.github.com>
2021-07-09 15:17:24 +0300

    Amharic in amharic characters

--- src/app/modules/bus/component/bus-tumb/bus-tumb.module.ts
+++ src/app/modules/bus/component/bus-tumb/bus-tumb.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BusTumbComponent } from './bus-tumb.component';
 import {FlexModule} from '@angular/flex-layout';
-import {CarPlateModule} from "./car-plate/car-plate.module";
+import {CarPlateModule} from './car-plate/car-plate.module';
 import {ShimmerModule} from '../../../common/shimmer/shimmer.module';
 import {LangModule} from '../../../../lang.module';
 
