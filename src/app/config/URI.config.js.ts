3a40b0f741fe70a434f59c16cb0658e37247ea16
eba-alemayehu <31594849+eba-alemayehu@users.noreply.github.com>
2021-07-12 21:27:32 +0300

    Dev uri

--- src/app/config/URI.config.js.ts
+++ src/app/config/URI.config.js.ts
@@ -4,7 +4,7 @@ let _SERVER_URI = 'https://hrkylbniai.execute-api.us-west-2.amazonaws.com/dev/';
 
 if (isDevMode()){
     // _SERVER_URI   = 'https://hrkylbniai.execute-api.us-west-2.amazonaws.com/dev/';
-    _SERVER_URI   = 'http://localhost:8000/';
+    _SERVER_URI   = 'https://hrkylbniai.execute-api.us-west-2.amazonaws.com/dev/';
 }
 
 export const SERVER_URI   = _SERVER_URI;
