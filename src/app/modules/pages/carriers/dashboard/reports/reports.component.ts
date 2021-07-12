e68979fa6cd2ebdc6f21981819e1e517b5da29ff
Muluken Bogale <mulukenb89@gmail.com>
2021-07-12 11:18:57 +0300

    fix generate report error

--- src/app/modules/pages/carriers/dashboard/reports/reports.component.ts
+++ src/app/modules/pages/carriers/dashboard/reports/reports.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {TripGQL} from '../../../../../generated/graphql';
+import {map, tap} from 'rxjs/operators';
+import {echo} from '../../../../../util/print';
 
 @Component({
   selector: 'app-reports',
@@ -8,7 +11,23 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor(public activatedRoute: ActivatedRoute) { }
+  fileName: string;
+  // TODO: trip id
+  constructor(public activatedRoute: ActivatedRoute, private tripGQL: TripGQL) {
+    this.activatedRoute.params.subscribe(
+      (params) => {
+        echo(params.id);
+        this.tripGQL.watch({id: 'VHJpcE5vZGU6MzQwMg=='}).valueChanges.pipe(
+          map(response => response.data.trip)).subscribe(
+          (trip) => {
+            echo(trip);
+            this.fileName = `${trip.route.leavingFrom.name}-to-${trip.route.destination.name}-${trip.departureTime}.xlsx`;
+            echo(this.fileName);
+          }
+        );
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
